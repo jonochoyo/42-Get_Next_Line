0247bab89b113f5f4a338388e3caf6810f4ee0ca
Jonathan Choy <jonochoyo@gmail.com>
2023-08-17 17:35:54 +0930

    Add main for printing lines on file

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: jchoy-me <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/15 15:34:08 by jchoy-me          #+#    #+#             */
-/*   Updated: 2023/08/16 17:02:20 by jchoy-me         ###   ########.fr       */
+/*   Updated: 2023/08/17 17:11:36 by jchoy-me         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,25 +15,43 @@
 /*
 Attempting to read BUFFER_SIZE from a text file. 
 If the fd is negative or the BUFFER_SIZE is <= 0, return NULL as can't read. 
+If the return value of read is <= 0 mean we reached the EOF or we got an error.
 */
 
 char	*get_next_line(int fd)
 {
 	char	*buffer;
+	int		bytes_read;
 
 	if (fd < 0 || BUFFER_SIZE <= 0)
 		return (NULL);
 	buffer = (char *) malloc(sizeof(char) * (BUFFER_SIZE + 1));
 	if (buffer == NULL)
 		return (NULL);
-	read(fd, buffer, BUFFER_SIZE);
+	bytes_read = read(fd, buffer, BUFFER_SIZE);
+	if (bytes_read <= 0)
+		return (NULL);
 	return (buffer);
 }
 
+/*
+Test with main to keep reading until it reaches the end of file.
+*/
+
 int	main(void)
 {
-	int	fd;
+	char	*line;
+	int		fd;
 
 	fd = open("test.txt", O_RDONLY);
-	printf("%s", get_next_line(fd));
+	line = get_next_line(fd);
+	printf("--");
+	while (line != NULL)
+	{
+		printf("%s--", line);
+		free(line);
+		line = get_next_line(fd);
+	}
+	close(fd);
+	return (0);
 }
