2ef83f6b9df6eea3742238cd207ba75c1347a1b6
Jonathan Choy <jonochoyo@gmail.com>
2023-08-18 17:59:06 +0930

    Add helper functions and utils

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: jchoy-me <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/15 15:34:08 by jchoy-me          #+#    #+#             */
-/*   Updated: 2023/08/17 17:11:36 by jchoy-me         ###   ########.fr       */
+/*   Updated: 2023/08/18 17:55:08 by jchoy-me         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,40 +18,131 @@ If the fd is negative or the BUFFER_SIZE is <= 0, return NULL as can't read.
 If the return value of read is <= 0 mean we reached the EOF or we got an error.
 */
 
+/*
+save_full_line will get a full line including '\n' by reading the file
+and saving the bytes into the buffer as many times as needed. it stops once
+get a full line.
+*/
+
+char	*save_full_line(int fd)
+{
+	char	*full_line;
+	int		bytes_read;
+	int		line_completed;
+	char	*buffer;
+
+	line_completed = 0;
+	full_line = ft_strdup("\0");
+	while (line_completed == 0)
+	{
+		buffer = (char *) malloc(sizeof(char) * (BUFFER_SIZE + 1));
+		if (buffer == NULL)
+		{
+			free(full_line);
+			return (NULL);
+		}	
+		bytes_read = read(fd, buffer, BUFFER_SIZE);
+		if (bytes_read <= 0)
+		{
+			free(buffer);
+			free(full_line);
+			return (NULL);
+		}
+		buffer[BUFFER_SIZE] = '\0';
+		if (ft_strchr(buffer, '\n') != 0)
+			line_completed = 1;
+		full_line = ft_strjoin(full_line, buffer);
+		free(buffer);
+	}
+	return (full_line);
+}
+
+/*
+Pass the full line to the helper function and get a substring from it up 
+to and including the \n.
+*/
+
+char	*get_cropped_line(char	*full_line)
+{
+	size_t	i;
+	char	*cropped_line;
+
+	i = 0;
+	if (full_line == NULL)
+		return (NULL);
+	while (full_line[i] != '\n' && full_line[i] != '\0')
+	{
+		i++;
+	}
+	i++;
+	cropped_line = ft_substr(full_line, 0, i);
+	return (cropped_line);
+}
+
+char	*get_line_remainder(char *full_line)
+{
+	size_t	i;
+	int		start;
+	int		remain_len;
+	char	*line_remainder;
+
+	i = 0;
+	if (full_line == NULL)
+		return (NULL);
+	while (full_line[i] != '\n' && full_line[i] != '\0')
+	{
+		i++;
+	}
+	i++;
+	start = i;
+	remain_len = ft_strlen(full_line) - start + 1;
+	line_remainder = ft_substr(full_line, start, remain_len);
+	if (line_remainder == NULL)
+		return (NULL);
+	return (line_remainder);
+}
+
 char	*get_next_line(int fd)
 {
-	char	*buffer;
-	int		bytes_read;
+	char	*full_line;
+	char	*cropped_line;
+	static char	*remaining_line;
+	char	*return_line;
 
 	if (fd < 0 || BUFFER_SIZE <= 0)
 		return (NULL);
-	buffer = (char *) malloc(sizeof(char) * (BUFFER_SIZE + 1));
-	if (buffer == NULL)
+	full_line = save_full_line(fd);
+	if (full_line == NULL)
 		return (NULL);
-	bytes_read = read(fd, buffer, BUFFER_SIZE);
-	if (bytes_read <= 0)
-		return (NULL);
-	return (buffer);
+	cropped_line = get_cropped_line(full_line);
+	if (remaining_line != NULL)
+		return_line = ft_strjoin(remaining_line, cropped_line);
+	else
+		return_line = ft_strdup(cropped_line);
+	remaining_line = get_line_remainder(full_line);
+	free(full_line);
+	free(cropped_line);
+	return (return_line);
 }
 
 /*
 Test with main to keep reading until it reaches the end of file.
 */
 
-int	main(void)
-{
-	char	*line;
-	int		fd;
+// int	main(void)
+// {
+// 	char	*line;
+// 	int		fd;
 
-	fd = open("test.txt", O_RDONLY);
-	line = get_next_line(fd);
-	printf("--");
-	while (line != NULL)
-	{
-		printf("%s--", line);
-		free(line);
-		line = get_next_line(fd);
-	}
-	close(fd);
-	return (0);
-}
+// 	fd = open("test.txt", O_RDONLY);
+// 	line = get_next_line(fd);
+// 	printf("--");
+// 	while (line != NULL)
+// 	{
+// 		printf("%s--", line);
+// 		free(line);
+// 		line = get_next_line(fd);
+// 	}
+// 	close(fd);
+// 	return (0);
+// }
--- get_next_line_utils.c
+++ get_next_line_utils.c
@@ -6,8 +6,127 @@
 /*   By: jchoy-me <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/15 16:33:51 by jchoy-me          #+#    #+#             */
-/*   Updated: 2023/08/15 16:34:23 by jchoy-me         ###   ########.fr       */
+/*   Updated: 2023/08/18 16:16:51 by jchoy-me         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
+
+char	*ft_strchr(const char *s, int c)
+{
+	int	i;
+
+	i = 0;
+	while (s[i] != '\0')
+	{
+		if (s[i] == (char) c)
+			return ((char *) &(s[i]));
+		i++;
+	}
+	if (s[i] == (char) c)
+		return ((char *) &(s[i]));
+	return (NULL);
+}
+
+char	*ft_strdup(const char *s1)
+{
+	char	*ptr;
+	int		nbytes;
+
+	nbytes = ft_strlen(s1) + 1;
+	ptr = (char *) malloc(sizeof(char) * nbytes);
+	if (ptr == NULL)
+		return (NULL);
+	ft_strlcpy(ptr, s1, nbytes);
+	return (ptr);
+}
+
+char	*ft_strjoin(char const *s1, char const *s2)
+{
+	char	*dest;
+	size_t	nbytes;
+
+	if (s1 == NULL || s2 == NULL)
+		return (NULL);
+	nbytes = ft_strlen(s1) + ft_strlen(s2) + 1;
+	dest = (char *) malloc(sizeof(char) * nbytes);
+	if (dest == NULL)
+		return (NULL);
+	ft_strlcpy(dest, s1, ft_strlen(s1) + 1);
+	ft_strlcat(dest, s2, nbytes);
+	return (dest);
+}
+
+size_t	ft_strlcpy(char *dst, const char *src, size_t dstsize)
+{
+	size_t	i;
+
+	i = 0;
+	if (dstsize > 0)
+	{
+		while (src[i] != '\0' && i < dstsize - 1)
+		{
+			dst[i] = src[i];
+			i++;
+		}
+		dst[i] = '\0';
+	}
+	return (ft_strlen(src));
+}
+
+size_t	ft_strlcat(char *dst, const char *src, size_t dstsize)
+{
+	size_t	i;
+	size_t	j;
+
+	i = 0;
+	j = 0;
+	while (dst[i] != '\0' && (i < dstsize))
+		i++;
+	while ((i + j + 1) < dstsize && src[j] != '\0')
+	{
+		dst[i + j] = src[j];
+		j++;
+	}
+	if (i < dstsize)
+		dst[i + j] = '\0';
+	return (i + ft_strlen(src));
+}
+
+size_t	ft_strlen(const char *s)
+{
+	size_t	len;
+
+	len = 0;
+	while (s[len] != '\0')
+	{
+		len++;
+	}
+	return (len);
+}
+
+char	*ft_substr(char const *s, unsigned int start, size_t len)
+{
+	char	*sbstr;
+	size_t	i;
+	size_t	strsize;
+
+	i = 0;
+	if (s == NULL)
+		return (NULL);
+	strsize = ft_strlen(s);
+	if (len > strsize - start)
+		len = strsize - start;
+	if (start > strsize)
+		len = 0;
+	sbstr = (char *) malloc(sizeof(char) * (len + 1));
+	if (sbstr == NULL)
+		return (NULL);
+	while (i < len && s[start + i] != '\0' && start < strsize)
+	{
+		sbstr[i] = s[start + i];
+		i++;
+	}
+	sbstr[i] = '\0';
+	return (sbstr);
+}
